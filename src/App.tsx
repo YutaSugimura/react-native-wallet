de492c920a279870d2a71404fa2d9fffa09f31bc
YutaSugimura <nextc3m@gmail.com>
2021-04-27 08:54:11 +0900

    Add ethers.js

--- src/App.tsx
+++ src/App.tsx
@@ -9,61 +9,37 @@
  */
 
 import React, { useEffect, useState } from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-} from 'react-native';
-import crypto from 'crypto';
-import { generateMnemonic } from './scripts/mnemonic';
+import { SafeAreaView, ScrollView, StatusBar, Text, useColorScheme } from 'react-native';
+import { ethers } from 'ethers';
 
 const App: React.FC = () => {
   const isDarkMode = useColorScheme() === 'dark';
-  const [mnemonic, setMnemonic] = useState<string>('');
+  const [mnemonic, setMnemonic] = useState<string>('none');
 
-  const changeMnemonic = async () => {
-    const newMnemonic = await generateMnemonic();
-    setMnemonic(newMnemonic);
+  const getMnemonic = () => {
+    const wallet = ethers.Wallet.createRandom();
+    const newMnemonic = wallet.mnemonic;
+    setMnemonic(newMnemonic.phrase);
+
+    const hdnode = ethers.utils.HDNode.fromMnemonic(newMnemonic.phrase);
+    const nodeA = hdnode.derivePath("m/44'/60'/0'/0");
+    const nodeB = hdnode.derivePath("m/44'/60'/0'/1");
+    console.log('1', nodeA.address);
+    console.log('2', nodeB.address);
   };
 
   useEffect(() => {
-    changeMnemonic();
+    getMnemonic();
   }, []);
 
   return (
     <SafeAreaView>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
       <ScrollView contentInsetAdjustmentBehavior="automatic">
-        <Text style={styles.sectionDescription}>
-          Random: {crypto.randomBytes(32).toString('base64')}
-        </Text>
-
         <Text>Mnemonci: {mnemonic}</Text>
       </ScrollView>
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
