a0fb52862bce24836e7bb1845d66749f74ae8d85
YutaSugimura <nextc3m@gmail.com>
2021-04-26 22:21:35 +0900

    Update scripts/mnemonic

--- src/scripts/mnemonic.ts
+++ src/scripts/mnemonic.ts
@@ -1,6 +1,20 @@
-export const generateMnemonic = async () => {
+export const generateMnemonic = async (): Promise<string> => {
   const bip39 = require('react-native-bip39');
-  const newMnemonic = await bip39.generateMnemonic(256);
 
-  return newMnemonic;
+  try {
+    const newMnemonic: string = await bip39.generateMnemonic(256);
+    if (bip39.validateMnemonic(newMnemonic)) {
+      return newMnemonic;
+    }
+
+    return await generateMnemonic();
+  } catch {
+    return await generateMnemonic();
+  }
+};
+
+export const validateMnemonic = (mnemonic: string): boolean => {
+  const bip39 = require('react-native-bip39');
+
+  return bip39.validateMnemonic(mnemonic);
 };
